2581dade050a3abcc0877cbb61f34c5a22390089
Greg <kiza.soza@gmail.com>
2023-03-04 23:46:56 +1000

    init

--- components/seplos/seplos.cpp
+++ components/seplos/seplos.cpp
@@ -20,6 +20,7 @@ void SeplosComponent::setup() {
 void SeplosComponent::loop() {
   currentMillis = millis();
   //if ((currentMillis - startMillis >= period) || (firstgo==true))
+  ESP_LOGW(TAG, "no loop for you");
 
   if (currentMillis - startMillis >= period)  //test whether the period has elapsed
   {
