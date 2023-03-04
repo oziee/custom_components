e1557085352d0bacf7eb2770cbd57feb3cd93a7a
Greg <kiza.soza@gmail.com>
2023-03-04 19:33:25 +1000

    init

--- components/seplos/__init__.py
+++ components/seplos/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart
+from esphome.components import uart, sensor, text_sensor
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ['uart']
