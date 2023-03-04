70eb32c2af646e5b5a450626b74f317baa243bb7
Greg <kiza.soza@gmail.com>
2023-03-04 17:46:55 +1000

    init

--- components/seplos/seplos.cpp
+++ components/seplos/seplos.cpp
@@ -11,52 +11,49 @@ void SeplosComponent::dump_config() {
   LOG_TEXT_SENSOR("  ", "Charger Text", charger_text_sensor_);
   check_uart_settings(9600);
 }
-
+void SeplosComponent::start() {
+  startMillis = millis();
+}
 void SeplosComponent::loop() {
-  const uint32_t now = millis();
-  if ((state_ > 0) && (now - last_transmission_ >= 200)) {
-    // last transmission too long ago. Reset RX index.
-    ESP_LOGW(TAG, "Last transmission too long ago.");
-    state_ = 0;
-  }
+  currentMillis = millis();
 
-  if (!available())
-    return;
 
-  last_transmission_ = now;
-  while (available()) {
-    uint8_t c;
-    read_byte(&c);
-    if (state_ == 0) {
-      if ((c == '\r') || (c == '\n'))
-        continue;
-      label_.clear();
-      value_.clear();
-      state_ = 1;
-    }
-    if (state_ == 1) {
-      if (c == '\t')
-        state_ = 2;
-      else
-        label_.push_back(c);
-      continue;
-    }
-    if (state_ == 2) {
-      if (label_ == "Checksum") {
-        state_ = 0;
-        continue;
-      }
-      if ((c == '\r') || (c == '\n')) {
-        handle_value_();
-        state_ = 0;
-      } else {
-        value_.push_back(c);
-      }
+  if (currentMillis - startMillis >= period)  //test whether the period has elapsed
+  {
+    ESP_LOGW(TAG, "transmission availabe.");
+    if(available() > 1){
+      fetchSerial();
     }
   }
+    
+
 }
 
+void fetchSerial (){
+  int bytes_read = 0 ;
+  bool be = false;
+  uint8_t buffer[76];
+  char tmp[16];
 
+  while (bytes_read < 76)
+  {
+    if (available() > 0)
+    {
+      uint8_t RXX = read();
+      //wait for the starting byte to come in which is \xUFF (x55 x46 x46)
+      if(RXX == 0x55) {
+        be = true;
+      }
+      if (be==true) {
+        buffer[bytes_read] = RXX;
+        sprintf(tmp, "%.2X",buffer[bytes_read]);
+        value_ = value_ + tmp;
+        bytes_read ++;
+      }
+    }
+  }
+  handle_value_();
+}
 
 void SeplosComponent::handle_value_() {
   int value;
