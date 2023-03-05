495f817d2cc02eb225b4cc855366a7ba95c17afb
Greg <kiza.soza@gmail.com>
2023-03-05 13:58:11 +1000

    init

--- components/seplos_bms/seplos_bms.cpp
+++ components/seplos_bms/seplos_bms.cpp
@@ -1,18 +1,22 @@
 #include "seplos_bms.h"
 #include "esphome/core/log.h"
 #include <vector>
+#include "Crc16.h"
 
 namespace esphome {
 namespace seplos_bms {
 
 static const char *const TAG = "seplos_bms";
 
-static const uint8_t SEPLOS_FRAME_SIZE = 76; //13
+static const uint8_t SEPLOS_FRAME_SIZE = 76; 0-75 76 bytes
+static const uint8_t SEPLOS_START_BYTE = 0x55;
+static const uint8_t SEPLOS_END_BYTE = 0xaa;
+
 static const uint8_t SEPLOS_TEMPERATURE_OFFSET = 40;
 static const uint16_t SEPLOS_CURRENT_OFFSET = 30000;
 
-static const uint8_t SEPLOS_START_BYTE = 0x55;
-static const uint8_t SEPLOS_REQUEST_MIN_MAX_VOLTAGE = 0x91;
+
+// static const uint8_t SEPLOS_REQUEST_MIN_MAX_VOLTAGE = 0x91;
 // static const uint8_t SEPLOS_REQUEST_MIN_MAX_TEMPERATURE = 0x92;
 // static const uint8_t SEPLOS_REQUEST_MOS = 0x93;
 // static const uint8_t SEPLOS_REQUEST_STATUS = 0x94;
@@ -35,6 +39,38 @@ void SeplosBmsComponent::update() {
   // this->request_data_(SEPLOS_REQUEST_CELL_VOLTAGE);
   // this->request_data_(SEPLOS_REQUEST_TEMPERATURE);
 
+  // int bytes_read = 0 ;
+  // bool be = false;
+  // uint8_t buffer[76];
+  // char tmp[16];
+
+  // ESP_LOGW(TAG, "fetching data.");
+
+  // value_.clear();
+
+  // while (bytes_read < 76)
+  // {
+  //   if (available() > 0)
+  //   {
+  //     ESP_LOGW(TAG, "reading data.");
+  //     uint8_t RXX = read();
+  //     //wait for the starting byte to come in which is \xUFF (x55 x46 x46)
+  //     if(RXX == 0x55) {
+  //       be = true;
+  //     }
+  //     if (be==true) {
+  //       buffer[bytes_read] = RXX;
+  //       sprintf(tmp, "%.2X",buffer[bytes_read]);
+  //       value_ = value_ + tmp;
+  //       bytes_read ++;
+  //     }
+  //   }
+  // }
+
+  // get_seplos_data.push_back(0x01);
+  // auto it = get_seplos_data.begin();
+  // it = get_seplos_data.insert(it, 0x20);
+
   std::vector<uint8_t> get_seplos_data;
   int available_data = this->available();
   ESP_LOGW(TAG, "reading avalible size: %d", available_data);
@@ -48,54 +84,24 @@ void SeplosBmsComponent::update() {
 
 float SeplosBmsComponent::get_setup_priority() const { return setup_priority::DATA; }
 
-// void SeplosBmsComponent::request_data_(uint8_t data_id) {
-//   uint8_t request_message[SEPLOS_FRAME_SIZE];
-
-//   request_message[0] = 0x55;     // Start Flag
-//   request_message[1] = addr_;    // Communication Module Address
-//   request_message[2] = data_id;  // Data ID
-//   request_message[3] = 0x08;     // Data Length (Fixed)
-//   request_message[4] = 0x00;     // Empty Data
-//   request_message[5] = 0x00;     //     |
-//   request_message[6] = 0x00;     //     |
-//   request_message[7] = 0x00;     //     |
-//   request_message[8] = 0x00;     //     |
-//   request_message[9] = 0x00;     //     |
-//   request_message[10] = 0x00;    //     |
-//   request_message[11] = 0x00;    // Empty Data
-//   request_message[12] = (uint8_t)(request_message[0] + request_message[1] + request_message[2] +
-//                                   request_message[3]);  // Checksum (Lower byte of the other bytes sum)
-
-//   this->write_array(request_message, sizeof(request_message));
-//   this->flush();
-// }
-
 void SeplosBmsComponent::decode_data_(std::vector<uint8_t> data) {
   auto it = data.begin();
 
   ESP_LOGD("TAG", "decoding data");
 
-  //while ((it = std::find(it, data.end(), 0xA5)) != data.end()) { //0xA5 i belive to to start byte
-  while ((it = std::find(it, data.end(), SEPLOS_START_BYTE)) != data.end()) {
-    // for(int i = 0; i < 76; i++)
-    // {
-    //   ESP_LOGD("TAG", "it %d: %d",i,it[i]);
-    // } 
 
-    if (data.end() - it >= SEPLOS_FRAME_SIZE && it[0] == SEPLOS_START_BYTE && it[1] == 0x46 && it[75] == 0xaa) { //end byte?
+  while ((it = std::find(it, data.end(), SEPLOS_START_BYTE)) != data.end()) {
+    if (data.end() - it >= SEPLOS_FRAME_SIZE && it[0] == SEPLOS_START_BYTE && it[1] == 0x46 && it[75] == SEPLOS_END_BYTE) {
       ESP_LOGD("TAG", "advance 1");
+      ESP_LOGD("TAG", "CRC %f", (float)encode_uint16(it[73], it[74]));
+
       //std::advance(it, SEPLOS_FRAME_SIZE);
       break;
     } else {
-      ESP_LOGD("TAG", "too many items");
-      //std::advance(it, 1);
+      ESP_LOGD("TAG", "Data is not valid with 55 46 ..... 170");
       break;
     }
   }
-
-  
-
-  ESP_LOGD("TAG", "end of decode data");
 }
 
 }  // namespace seplos_bms
