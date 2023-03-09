b5e755513d5ffe51d3c3afea732d31388ab8b7ad
Greg <kiza.soza@gmail.com>
2023-03-09 12:09:01 +1000

    Update seplos_bms.cpp

--- components/seplos_bms/seplos_bms.cpp
+++ components/seplos_bms/seplos_bms.cpp
@@ -32,50 +32,43 @@ void SeplosBmsComponent::dump_config() {
 
 void SeplosBmsComponent::update() {
 
-  std::vector<uint8_t> get_seplos_data;
-  int available_data = this->available();
-  ESP_LOGW(TAG, "reading avalible size: %d", available_data);
-  if (available_data >= SEPLOS_FRAME_SIZE) {
-    get_seplos_data.resize(available_data);
-    this->read_array(get_seplos_data.data(), available_data);
-    this->decode_data_(get_seplos_data);
-  }
-
-  // int bytes_read = 0 ;
-  // bool be = false;
-  // //uint8_t buffer[76];
-  // //char tmp[16];
-
   // std::vector<uint8_t> get_seplos_data;
-  // //get_seplos_data.resize(75);
-
-  
-  // while (bytes_read < 76)
-  // {
-  //   if (available() > 0)
-  //   {
-      
-      
-  //     //uint8_t RXX = read_byte(1) ;//read();
-  //     //uint8_t RXX = this->read_byte();
-  //     uint8_t RXX;
-  //     this->read_byte(&RXX);
-  //     ESP_LOGW(TAG, "reading data %d", RXX);
-  //     //wait for the starting byte to come in which is \xUFF (x55 x46 x46)
-  //     if(RXX == 0x55) {
-  //       be = true;
-  //     }
-  //     if (be==true) {
-  //       get_seplos_data.push_back(RXX);
-  //       //buffer[bytes_read] = RXX;
-  //       //sprintf(tmp, "%.2X",buffer[bytes_read]);
-  //       //value_ = value_ + tmp;
-  //       bytes_read ++;
-  //     }
-  //   }
+  // int available_data = this->available();
+  // ESP_LOGW(TAG, "reading avalible size: %d", available_data);
+  // if (available_data >= SEPLOS_FRAME_SIZE) {
+  //   get_seplos_data.resize(available_data);
+  //   this->read_array(get_seplos_data.data(), available_data);
+  //   this->decode_data_(get_seplos_data);
   // }
 
-  // this->decode_data_(get_seplos_data);
+  int bytes_read = 0 ;
+  bool be = false;
+
+  std::vector<uint8_t> get_seplos_data;
+  get_seplos_data.resize(75);
+
+  while (bytes_read < 76)
+  {
+    if (available() > 0)
+    {
+      uint8_t RXX;
+      this->read_byte(&RXX);
+      ESP_LOGW(TAG, "reading data...");
+      //wait for the starting byte to come in which is \xUFF (x55 x46 x46)
+      if(RXX == 0x55) {
+        be = true;
+      }
+      if (be==true) {
+        get_seplos_data.push_back(RXX);
+        //buffer[bytes_read] = RXX;
+        //sprintf(tmp, "%.2X",buffer[bytes_read]);
+        //value_ = value_ + tmp;
+        bytes_read ++;
+      }
+    }
+  }
+
+  this->decode_data_(get_seplos_data);
 
 }
 
