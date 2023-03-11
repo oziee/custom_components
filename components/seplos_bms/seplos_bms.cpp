8d913fb88e46e9a8536d3b7d8d59da1bab3dc49a
Greg <kiza.soza@gmail.com>
2023-03-11 16:03:57 +1000

    Update seplos_bms.cpp

--- components/seplos_bms/seplos_bms.cpp
+++ components/seplos_bms/seplos_bms.cpp
@@ -43,7 +43,7 @@ void SeplosBmsComponent::update() {
   do{
     for(int i=0;i<76;i++)
     {
-      this->read(data[i]);
+      data[i]=read();
       //get_seplos_data.push_back(data[i]);
     }
   }while(this->read()==0x55);
