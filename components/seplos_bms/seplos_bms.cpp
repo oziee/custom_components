59c620cde13f86237c346a5648d3e8e9ef312bcc
Greg <kiza.soza@gmail.com>
2023-03-11 15:47:22 +1000

    Update seplos_bms.cpp

--- components/seplos_bms/seplos_bms.cpp
+++ components/seplos_bms/seplos_bms.cpp
@@ -139,7 +139,7 @@ void SeplosBmsComponent::convertDecToBin(int Dec, bool Bin[]) {
 }
 
 //bool SeplosBmsComponent::decode_data_(std::vector<uint8_t> data) {
-bool SeplosBmsComponent::decode_data_(uint8_t data) {
+bool SeplosBmsComponent::decode_data_(uint8_t data[]) {
   //ESP_LOGD("TAG", "Received this data:");
   //ESP_LOGD(TAG, "%s", format_hex_pretty(&data.front(), data.size()).c_str());
   // std::string str1;
